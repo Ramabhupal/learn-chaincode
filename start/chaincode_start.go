6061f43aa91425c1fc39eb1b7150227ca47ee6dc
Ramabhupal <ramabhupal2013@gmail.com>
2017-03-24 16:13:44 +0530

    Update chaincode_start.go

--- start/chaincode_start.go
+++ start/chaincode_start.go
@@ -120,6 +120,8 @@ func (t *SimpleChaincode) Init(stub shim.ChaincodeStubInterface, function string
 	fmt.Printf("Hope for bestPlan for the worst!!!!!!!")
 	fmt.Printf("Never let down")
 fmt.Printf("Always go up")
+fmt.Printf("Always go upppppppppppppppp")
+
 
        if len(args) != 1 {
 		return nil, errors.New("Incorrect number of arguments. Expecting 1")
